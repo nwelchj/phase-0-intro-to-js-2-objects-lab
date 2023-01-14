42503b9a68db059bdc3d3f407f673f7f0b80d696
Nolan Welch <bugygamer.mc@gmail.com>
2023-01-13 19:22:26 -0700

    commit tree 3bb3cd373356ee4b8105e5c0886586a58620a8c6

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {            //there are four keys street,city,state, and zip 
+    Worker: {                  //the first key points points to another object 
+      name: "bod",
+      streetaddress: "1005",      //if the keys the are the same only the final vaule 
+    },                
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  const newemployee = { ...employee };
+
+  newemployee[key] = value;
+
+  return newemployee;
+}
+ 
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  employee[key] = value;
+
+  return employee;
+}
+
+function deleteFromEmployeeByKey (employee, key ){
+  const newemployee = { ...employee };
+
+  delete newemployee.Worker;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  return employee;
+  
+}
